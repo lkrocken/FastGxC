81be08ce9aeba1343c7a2c121705d75d33e07bbe
lkrocken <lkrocken@g.ucla.edu>
2024-11-28 00:24:28 -0800

    first commit

---
+++ R/decompose.R
@@ -0,0 +1,54 @@
+#' Decomposition Step
+#'
+#' Function to decompose expression into one shared component and specific components per context
+#'
+#' @param  input filepath where merged expression matrix is stored, output directory to store data
+#' @return outputs one file with the shared component of expression per individual and C files for each specific expression component for each of the C contexts
+#'
+#' @export
+decomposition_step = function(exp_mat_filename, data_dir){
+if(!dir.exists(data_dir)) dir.create(data_dir)
+#%%%%%%%%%%%%%%% Read expression matrix, genes in columns, samples in rows.
+#exp_mat=read.table(file = paste0(data_dir,exp_mat_filename), sep = '\t')
+exp_mat=data.table::fread(file = exp_mat_filename, sep = '\t', data.table = F)
+
+#%%%%%%%%%%%%%%% Print number of genes and samples
+sprintf("There are %s samples and %s genes. The max number of missing samples for a gene is  %s. The max number of missing genes for a sample is  %s.", nrow(exp_mat), ncol(exp_mat),max(colSums(is.na(exp_mat))),max(rowSums(is.na(exp_mat))))
+
+#%%%%%%%%%%%%%%% Sample and context names
+design=sapply(1:nrow(exp_mat), function(i) unlist(strsplit(exp_mat[,1][i], split = " - "))[1])
+context_names=sapply(1:nrow(exp_mat), function(i) unlist(strsplit(exp_mat[,1][i], split = " - "))[2])
+contexts=unique(context_names)
+
+#%%%%%%%%%%%%%%% Decompose expression into homogeneous and heterogeneous context expression
+print("Decomposing data")
+rownames(exp_mat) = exp_mat[,1]
+exp_mat = exp_mat[,-1]
+dec_exp_all=decompose(X = exp_mat, design = design)
+bexp_all=dec_exp_all$Xb
+wexp_all=dec_exp_all$Xw
+bexp_all[is.nan(bexp_all)]=NA
+wexp_all[is.nan(wexp_all)]=NA
+
+sprintf("Between individual matrix: There are %s individuals and %s genes. The max number of missing samples for a gene is  %s. The max number of missing genes for a sample is  %s.", nrow(bexp_all), ncol(bexp_all),max(colSums(is.na(bexp_all))),max(rowSums(is.na(bexp_all))))
+
+sprintf("Within individual matrix: There are %s samples and %s genes. The max number of missing samples for a gene is  %s. The max number of missing genes for a sample is  %s.", nrow(wexp_all), ncol(wexp_all),max(colSums(is.na(wexp_all))),max(rowSums(is.na(wexp_all))))
+
+#%%%%%%%%%%%%%%% Save decomposed expression files 
+print("Finished decomposition, saving files")
+
+print("Saving between-individuals variation matrix")
+fwrite(x = data.table::data.table(t(bexp_all),keep.rownames = T) %>% {setnames(., old = "rn", new = "geneID")[]},
+       file = paste0(data_dir,"context_shared_expression.txt"), quote = F, row.names = F,
+       col.names = T, append = F, sep = '\t')
+
+print("Saving within-individuals variation matrix for context: ")
+for(i in 1:length(contexts)){
+  print(contexts[i])
+  wexp_t = wexp_all[grep(pattern = paste0(contexts[i],"$"), rownames(wexp_all)),]
+  rownames(wexp_t)=gsub(pattern = paste0(" - ",contexts[i]), replacement = "", x = rownames(wexp_t))
+  fwrite(x = data.table::data.table(t(wexp_t),keep.rownames = T) %>% {setnames(., old = "rn", new = "geneID")[]},
+         file = paste0(data_dir,contexts[i],"_specific_expression.txt"),quote = F, row.names = F,
+         col.names = T, append = F, sep = '\t')
+}
+}
---
+++ R/eQTL_mapping.R
@@ -0,0 +1,90 @@
+#' eQTL Mapping Step
+#'
+#' Function to map cis eQTLs
+#'
+#' @param  SNP_file_name - full file path with genotypes of all individuals
+#' @param  snps_location_file_name - full file path with snp ids, start, and end positions
+#' @param  expression_file_name - full file path with expression matrix of individuals per gene
+#' @param  gene_location_file_name - full file path with gene ids, start, and end positions
+#' @param  output_file_name - full file path of output file where eQTL files will be written out
+#' @return outputs one file of mapped cis eQTLs
+#'
+#' @export
+eQTL_mapping_step = function(SNP_file_name, snps_location_file_name, expression_file_name, gene_location_file_name, output_file_name){
+
+# Output file name
+output_file_name_cis = output_file_name; 
+output_file_name_tra = tempfile();
+
+setDTthreads(1)
+print(paste0("data.table getDTthreads(): ",getDTthreads()))
+
+sprintf("Running analysis for %s", expression_file_name)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%% MatrixEQTL parameters
+#%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Linear model to use
+useModel = modelLINEAR; 
+
+# Only associations significant at this level will be saved
+pvOutputThreshold_cis = 1; 
+pvOutputThreshold_tra = 0;
+
+# Error covariance matrix
+errorCovariance = numeric();
+
+# Distance for local gene-SNP pairs
+cisDist = 1e6;
+
+#%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%% Read files
+#%%%%%%%%%%%%%%%%%%%%%%%%
+
+## Raw gene expression data with gene position
+expression_mat=as.matrix(data.frame(data.table::fread(input = expression_file_name, header = T),row.names = 1, check.names = F))
+genepos = read.table(file = gene_location_file_name, header = TRUE, stringsAsFactors = FALSE)[,1:4];
+
+## Genotype data with snp position
+snps = SlicedData$new();
+snps$fileDelimiter = "\t";      # the TAB character
+snps$fileOmitCharacters = "NA"; # denote missing values;
+snps$fileSkipRows = 1;          # one row of column labels
+snps$fileSkipColumns = 1;       # one column of row labels
+snps$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+snps$LoadFile(SNP_file_name);
+
+snpspos = read.table(file = snps_location_file_name, header = TRUE, stringsAsFactors = FALSE)[,1:3];
+
+## Make sure order of individuals is the same in gene expression and genotype matrices  
+snps$ColumnSubsample(which(colnames(snps) %in% colnames(expression_mat))) # Match SNP and expression individuals
+expression_mat=expression_mat[,colnames(snps)]
+gene = SlicedData$new();
+gene$CreateFromMatrix(expression_mat)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%% Run the analysis
+#%%%%%%%%%%%%%%%%%%%%%%%%
+
+me = Matrix_eQTL_main(
+  snps = snps,
+  gene = gene,
+  cvrt = SlicedData$new(),
+  output_file_name  = output_file_name_tra,
+  pvOutputThreshold  = pvOutputThreshold_tra,
+  useModel = useModel,
+  errorCovariance = errorCovariance,
+  verbose = TRUE,
+  output_file_name.cis = output_file_name_cis,
+  pvOutputThreshold.cis = pvOutputThreshold_cis,
+  snpspos = snpspos,
+  genepos = genepos,
+  cisDist = cisDist,
+  pvalue.hist = FALSE,
+  min.pv.by.genesnp = FALSE,
+  noFDRsaveMemory = FALSE);
+
+## Results:
+cat('Analysis finished in: ', me$time.in.sec, ' seconds', '\n')
+}
